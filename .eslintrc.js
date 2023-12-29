212d6c0f4894703136010e5240d7b5dd94debfd5
Devlin Duldulao <webmasterdevlin@gmail.com>
2023-12-29 21:07:34 +0100

    Update .eslintrc.js configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ['plugin:astro/recommended'],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:astro/recommended"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     tsconfigRootDir: __dirname,
-    sourceType: 'module',
-    ecmaVersion: 'latest'
+    sourceType: "module",
+    ecmaVersion: "latest",
   },
   overrides: [
     {
-      files: ['*.astro'],
-      parser: 'astro-eslint-parser',
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
       parserOptions: {
-        parser: '@typescript-eslint/parser',
-        extraFileExtensions: ['.astro', '.tsx', '.ts', '.jsx', '.js']
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro", ".tsx", ".ts", ".jsx", ".js", "md"],
       },
       rules: {
         // override/add rules settings here, such as:
         // "astro/no-set-html-directive": "error"
-      }
-    }
-  ]
-}
+      },
+    },
+  ],
+};
