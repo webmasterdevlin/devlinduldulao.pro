0e326046701c9ade7ea8b9c9812b050dac493b32
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-03-13 17:11:09 +0100

    Add new conferences to CFP list

--- src/content/cfps.ts
+++ src/content/cfps.ts
@@ -108,6 +108,16 @@ const conferences: Cfp[] = [
     location: "Porto, Portugal",
   },
   {
+    url: "https://ndcoslo.com",
+    month: "June",
+    location: "Oslo, Norway",
+  },
+  {
+    url: "https://cphdevfest.com",
+    month: "October",
+    location: "Copenhagen, Denmark",
+  },
+  {
     url: "https://portotechhub.com",
     month: "October",
     location: "Porto, Portugal",
@@ -149,7 +159,7 @@ const conferences: Cfp[] = [
   },
   {
     url: "https://www.developer-week.de",
-    month: "Juli",
+    month: "July",
     location: "Nuremberg, Germany",
   },
   {
@@ -202,8 +212,222 @@ const conferences: Cfp[] = [
     month: "November",
     location: "Mons, Belgium",
   },
+  {
+    url: "https://oredev.org",
+    month: "November",
+    location: "Malmö, Sweden",
+  },
+  {
+    url: "https://london.cityjsconf.org",
+    month: "April",
+    location: "London, UK",
+  },
+  {
+    url: "https://greece.cityjsconf.org",
+    month: "June",
+    location: "Athens, Greece",
+  },
+  {
+    url: "https://medellin.cityjsconf.org",
+    month: "October",
+    location: "Medellin, Colombia",
+  },
+  {
+    url: "https://berlin.cityjsconf.org",
+    month: "November",
+    location: "Berlin, Germany",
+  },
+  {
+    url: "https://nigeria.cityjsconf.org",
+    month: "October",
+    location: "Lagos, Nigeria",
+  },
+  {
+    url: "https://india.cityjsconf.org",
+    month: "December",
+    location: "New Delhi, India",
+  },
+  {
+    url: "https://belgrade.cityjsconf.org",
+    month: "September",
+    location: "Belgrade, Serbia",
+  },
+  {
+    url: "https://singapore.cityjsconf.org",
+    month: "July",
+    location: "Singapore",
+  },
+  {
+    url: "https://brazil.cityjsconf.org",
+    month: "August",
+    location: "São Paulo, Brazil",
+  },
+  {
+    url: "https://techorama.nl",
+    month: "October",
+    location: "Ede, Netherlands",
+  },
+  {
+    url: "https://techorama.be",
+    month: "May",
+    location: "Antwerp, Belgium",
+  },
+  {
+    url: "https://react.paris",
+    month: "March",
+    location: "Paris, France",
+  },
+  {
+    url: "https://www.react.brussels",
+    month: "October",
+    location: "Brussels, Belgium",
+  },
+  {
+    url: "https://www.bejs.io",
+    month: "May",
+    location: "Brussels, Belgium",
+  },
+  {
+    url: "https://reactday.berlin",
+    month: "December",
+    location: "Berlin, Germany",
+  },
+  {
+    url: "https://jsheroes.io",
+    month: "May",
+    location: "Cluj-Napoca, Romania",
+  },
+  {
+    url: "https://revojs.ro",
+    month: "October",
+    location: "Timisoara, Romania",
+  },
+  {
+    url: "https://jsconfbp.com",
+    month: "June",
+    location: "Budapest, Hungary",
+  },
+  {
+    url: "https://www.buildstuff.events",
+    month: "November",
+    location: "Vilnius, Lithuania",
+  },
+  {
+    url: "https://devoxx.be",
+    month: "October",
+    location: "Antwerp, Belgium",
+  },
+  {
+    url: "https://devoxx.ma",
+    month: "October",
+    location: "Morocco",
+  },
+  {
+    url: "https://www.devoxx.co.uk",
+    month: "May",
+    location: "London, UK",
+  },
+  {
+    url: "https://devoxx.gr",
+    month: "April",
+    location: "Athens, Greece",
+  },
+  {
+    url: "https://www.devoxx.fr",
+    month: "April",
+    location: "Paris, France",
+  },
+  {
+    url: "https://voxxeddays.com/thessaloniki",
+    month: "October",
+    location: "Thessaloniki, Greece",
+  },
+  {
+    url: "https://luxembourg.voxxeddays.com",
+    month: "June",
+    location: "Luxembourg",
+  },
+  {
+    url: "https://voxxeddays.com/trieste",
+    month: "May",
+    location: "Trieste, Italy",
+  },
+  {
+    url: "https://brussels.voxxeddays.com",
+    month: "May",
+    location: "Brussels, Belgium",
+  },
+  {
+    url: "https://romania.voxxeddays.com",
+    month: "March",
+    location: "Bucharest, Romania",
+  },
+  {
+    url: "https://frontend.barcelona",
+    month: "December",
+    location: "Barcelona, Spain",
+  },
+  {
+    url: "https://nordicjs.com",
+    month: "October",
+    location: "Stockholm, Sweden",
+  },
+  {
+    url: "https://javascript-conference.com/new-york",
+    month: "October",
+    location: "New York, USA",
+  },
+  {
+    url: "https://www.jsconf.ie",
+    month: "September",
+    location: "Dublin, Ireland",
+  },
+  {
+    url: "https://www.wearedevelopers.com/world-congress",
+    month: "July",
+    location: "Berlin, Germany",
+  },
+  {
+    url: "https://appjs.co",
+    month: "May",
+    location: "Krakow, Poland",
+  },
+  {
+    url: "https://www.reactmiami.com",
+    month: "April",
+    location: "Miami, USA",
+  },
+  {
+    url: "https://weblica.hr/en/weblica",
+    month: "May",
+    location: "Zagreb, Croatia",
+  },
+  {
+    url: "https://laravellive.uk",
+    month: "June",
+    location: "London, UK",
+  },
+  {
+    url: "https://www.jfokus.se",
+    month: "May",
+    location: "Stockholm, Sweden",
+  },
+  {
+    url: "https://phpconference.nl",
+    month: "March",
+    location: "Amsterdam, Netherlands",
+  },
+  {
+    url: "https://webdevcon.nl",
+    month: "March",
+    location: "Amsterdam, Netherlands",
+  },
+  {
+    url: "https://appdevcon.nl",
+    month: "March",
+    location: "Amsterdam, Netherlands",
+  },
 ];
-// TODO: NDC, Devoxx, Voxx, CityJS,
 export default conferences;
 
 export type Cfp = { month: string } & Pick<Talk, "location" | "url">;
