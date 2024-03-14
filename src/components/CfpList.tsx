0b843aa6f37107b7707f550429490d864b77d552
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-03-14 23:55:22 +0100

    Add tailwind-merge package and update CfpList component

--- src/components/CfpList.tsx
+++ src/components/CfpList.tsx
@@ -2,14 +2,16 @@ import React, { Fragment, useState } from "react";
 import CfpCard from "./CfpCard";
 import cfps from "@content/cfps";
 import getMonthsFromNow from "@utils/getMonthsFromNow";
+import { cn } from "@utils/style";
+
+const fourthMonth = getMonthsFromNow(4);
+const fifthMonth = getMonthsFromNow(5);
+const sixthMonth = getMonthsFromNow(6);
 
 export default function CfpList() {
   const [list, setList] = useState(cfps);
 
   const handleFilter = () => {
-    const fourthMonth = getMonthsFromNow(4);
-    const fifthMonth = getMonthsFromNow(5);
-    const sixthMonth = getMonthsFromNow(6);
     const filteredList = cfps.filter(
       cfp =>
         cfp.month === fourthMonth ||
@@ -59,7 +61,29 @@ export default function CfpList() {
         .map(cfp => {
           return (
             <Fragment key={cfp.url}>
-              <CfpCard {...cfp} />
+              <div
+                className={cn(
+                  (cfp.month === fourthMonth ||
+                    cfp.month === fifthMonth ||
+                    cfp.month === sixthMonth) &&
+                    "animate-pulse",
+                  "bg-white dark:bg-gray-800 mb-5 rounded-lg p-5 shadow-lg"
+                )}
+              >
+                <h3 className="text-cyan-600 dark:text-cyan-400 text-lg font-semibold">
+                  {cfp.location}
+                </h3>
+                <p className="text-gray-600 dark:text-gray-300 text-sm">
+                  {cfp.month}
+                </p>
+                <a
+                  href={cfp.url}
+                  target="_blank"
+                  className="text-cyan-600 dark:text-cyan-400 hover:underline"
+                >
+                  {cfp.url}
+                </a>
+              </div>
             </Fragment>
           );
         })}
