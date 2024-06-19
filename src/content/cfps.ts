1f5d3bc0bc4e1f4200d8c52186d95fc04f11d1d4
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-06-19 09:18:41 +0200

    feat: Add Front Conference 2022 to CFPs

--- src/content/cfps.ts
+++ src/content/cfps.ts
@@ -2,6 +2,11 @@ import type { Talk } from "./talks";
 
 const conferences: Cfp[] = [
   {
+    url: "https://frontconference.com",
+    month: "August",
+    location: "Zurich, Switzerland",
+  },
+  {
     url: "https://phpsrbija.rs",
     month: "October",
     location: "Belgrade, Serbia",
