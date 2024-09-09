70b7a4a0e1520682adb0e363cf4b5cbe5c671a75
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-09-09 12:47:58 +0200

    migrate eslint to v9

---
+++ eslint.config.mjs
@@ -0,0 +1,50 @@
+import tsParser from "@typescript-eslint/parser";
+import parser from "astro-eslint-parser";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [{
+    ignores: [
+        "**/.husky",
+        "**/.vscode",
+        "**/node_modules",
+        "**/public",
+        "**/dist",
+        "**/.yarn",
+    ],
+}, ...compat.extends("plugin:astro/recommended"), {
+    languageOptions: {
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module",
+
+        parserOptions: {
+            tsconfigRootDir: "/Users/devlinduldulao/Documents/DEVELOPMENT/Astro/devlinduldulao.pro",
+        },
+    },
+}, {
+    files: ["**/*.astro"],
+
+    languageOptions: {
+        parser: parser,
+        ecmaVersion: 5,
+        sourceType: "script",
+
+        parserOptions: {
+            parser: "@typescript-eslint/parser",
+            extraFileExtensions: [".astro", ".tsx", ".ts", ".jsx", ".js", "md"],
+        },
+    },
+
+    rules: {},
+}];
