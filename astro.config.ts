d1ed2c7b072d28885baf2f428756f4be2c05a69c
Devlin Duldulao <webmasterdevlin@gmail.com>
2024-09-24 09:53:33 +0200

    add pwa

--- astro.config.ts
+++ astro.config.ts
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs"; // make sure your relative path is correct
 import vercelStatic from "@astrojs/vercel/static";
-
+import AstroPWA from "@vite-pwa/astro";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
@@ -20,6 +20,34 @@ export default defineConfig({
     react(),
     sitemap(),
     partytown(),
+    AstroPWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Devlin Duldulao",
+        short_name: "devlinduldulao",
+        description: "my personal blog",
+        display: "standalone",
+        background_color: "peachpuff",
+        theme_color: "rgb(255 218 185)",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+        screenshots: [],
+        start_url: "/",
+      },
+    }),
   ],
   markdown: {
     remarkPlugins: [
--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,13 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+/// <reference types="vite-plugin-pwa/info" />
+
+declare module "virtual:pwa-register" {
+  import type { RegisterSWOptions } from "vite-plugin-pwa/types";
+
+  export type { RegisterSWOptions };
+
+  export function registerSW(
+    options?: RegisterSWOptions
+  ): (reloadPage?: boolean) => Promise<void>;
+}
